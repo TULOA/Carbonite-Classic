fabc88ecc823727da4d72767e65c8901b225637a
TULOA <ajgravlin_master@yahoo.com>
2020-06-24 12:53:33 -0500

    Add files via upload

---
+++ Carbonite.Quests/Data/QuestItems.lua
@@ -0,0 +1,6 @@
+Nx.QItems = {}  --Array to hold list of items used in quests.
+-- Format: [1]: ItemLink. Can be from wowhead. [2]: ItemID. Can be copied from ItemLink
+--TODO: Add [3]: Charges. Can get this from wowhead. Might consider a function from in game as well.
+Nx.QItems[4763] = 12346
+Nx.QItems[5321] = 13536
+Nx.QItems[1016] = 5456
